8f370489512c4d980032daab65cd16766f558c83
jameslunatic <jjluna@gbox.adnu.edu.ph>
2023-07-10 06:30:22 +0800

    error: FCM returns object Object

--- functions/index.js
+++ functions/index.js
@@ -1,68 +1,56 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-//    const {onRequest} = require("firebase-functions/v2/https");
-//    const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 admin.initializeApp();
 
-const refD = functions.database.ref("/Devices/0420");
+const refDev = functions.database.ref("Devices/{deviceId}");
+//  const refUse = functions.database.ref("Users/{userId}/token");
 
-exports.sendNotif = refD.onUpdate((change, context) => {
-  const newData = change.after.val();
-  const prevData = change.before.val();
+exports.sendNotif = refDev.onWrite(async (change, context) => {
+  const userId = context.params.userId;
+  //const FCMToken = await admin.database().ref(`Users/${userId}`).once('value');
+  const FCMTokenSnapshot = await admin.database().ref(`Users/${userId}/token`).once('value');
+  const FCMToken = FCMTokenSnapshot.val(); // Get the token value from the snapshot
 
-  const airTempCheck = newData.Temperature >= 35 && prevData.Temperature < 18;
-  const humidCheck = newData.Humidity >= 85 && prevData.Humidity < 50;
-  const watTempCheck = newData.WaterTemperature >= 28 && prevData.WaterTemperature < 20;
-  const tdsCheck = newData.TotalDissolvedSolids >= 1500 && prevData.TotalDissolvedSolids < 400;
-  const acidCheck = newData.pH >= 6.5 && prevData.pH < 5.0;
-  //  const stringCondition = newData.child2 === 'trigger' && prevData.child2 !== 'trigger';
+  const airTemp = change.after.child("Temperature").val();
+  const waterTemp = change.after.child("WaterTemperature").val();
+  const humidity = change.after.child("Humidity").val();
+  const tds = change.after.child("TotalDissolvedSolids").val();
+  const acidity = change.after.child("pH").val();
+  console.log(airTemp + ' ' + waterTemp + ' ' + FCMTokenSnapshot);
+  
+  //const tokens = ['d2mnsjvQQKuD0td89gfFAD:APA91bE3bm09uzPZgogCSiZpx5HZRPrhwDNuWPDzzgr4JjnuhPBcwCZscBbn7uGCglPwCopmTy2yPlcRcez0K_9qKF5eybkY8NbjiioXda-An4fb6o-Uo4Crwh5EioyuhsvjbLdWFc0k', 'eLLBXN2MTXSzcKTC4jHIxD:APA91bG91zHQZe4ZfwFLBR1bbHg0I34tBerODUZXQLEuxQz6Wm6TlGsHfWJrMxJzG4Cnb0rkZjmOPMWh1ivsITD1bA8Z6gMRPraZYDNOFCbzaBuW1Couj58yWL97n2Eygs4fvJoMAm0j', 'cbq9-IJGRCK9AxYqP1VrJm:APA91bEEhxnbAmg_CWWYiadDAZQzrWkeBSnJSdtDIM4fI2GMqlZN78WGAikBoWathrHF8A16zxOnSSbigifGujcWqrvtWdRERD9oeLRapa9bx1KVMNMu38Or_XPMjcM3SKRPa4UGRkUW'];
+
+  const airTempCheck = airTemp >= 35 || airTemp < 18;
+  const humidCheck = humidity >= 85 || humidity < 50;
+  const watTempCheck = waterTemp >= 28 || waterTemp < 20;
+  const tdsCheck = tds >= 1500 || tds < 400;
+  const acidCheck = acidity >= 6.5 || acidity < 5.0;
 
   if (airTempCheck || humidCheck || watTempCheck || tdsCheck || acidCheck) {
-    const message = {
+    const payload = {
+      token: FCMToken,
       notification: {
-        title: "Your plants are in danger!",
-        body: "Check on your reservoir and follow the suggestion to save them!",
-        sound: "dhnotif",
+        title: 'Your plants are in danger!',
+        body: 'Check on your reservoir and follow the suggestion to save them!',
+        sound: "beep",
         channel_id: "DigiHydro",
+        //token: "eLLBXN2MTXSzcKTC4jHIxD:APA91bG91zHQZe4ZfwFLBR1bbHg0I34tBerODUZXQLEuxQz6Wm6TlGsHfWJrMxJzG4Cnb0rkZjmOPMWh1ivsITD1bA8Z6gMRPraZYDNOFCbzaBuW1Couj58yWL97n2Eygs4fvJoMAm0j",
         priority: "high",
       },
-      topic: "notifications",
+      //topic: "notifications",
       //  Replace with the topic you want to send the notification to
       data: {
         //  Optional data payload to send additional info to the Flutter app
-        click_action: "FLUTTER_NOTIFICATION_CLICK",
+        click_action: 'FLUTTER_NOTIFICATION_CLICK',
       },
     };
+    //console.log(FCMToken);
 
-    return admin.messaging().send(message)
-        .then(() => {
-          console.log("Notification sent successfully.");
-          return null;
-        })
-        .catch((error) => {
-          console.error("Error sending notification:", error);
-          return null;
-        });
-  }
-
-  return null;
+    admin.messaging().send(payload).then(async (response) => {
+      console.log('Notification sent successfully', response);
+      return null;
+    }).catch((error) => {
+      return {error: error.code};
+    });
+  };
 });
