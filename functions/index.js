a8d2fd6d4a0a3d1f9d266363e0a9ec0ba9555948
jameslunatic <jjluna@gbox.adnu.edu.ph>
2023-07-11 00:00:54 +0800

    Single function notification: functional

--- functions/index.js
+++ functions/index.js
@@ -6,19 +6,19 @@ const refDev = functions.database.ref("Devices/{deviceId}");
 //  const refUse = functions.database.ref("Users/{userId}/token");
 
 exports.sendNotif = refDev.onWrite(async (change, context) => {
-  const userId = context.params.userId;
+  //const userId = context.params.userId;
   //const FCMToken = await admin.database().ref(`Users/${userId}`).once('value');
-  const FCMTokenSnapshot = await admin.database().ref(`Users/${userId}/token`).once('value');
+  const FCMTokenSnapshot = await admin.database().ref('Users/jVze0VP0DtSEo42WT9o0ghFHmwP2/token').once('value');
   const FCMToken = FCMTokenSnapshot.val(); // Get the token value from the snapshot
 
+  //jam: jVze0VP0DtSEo42WT9o0ghFHmwP2
+  //baj: WDYkjIUSyGX2FuuwxqEZOSPoZX72
   const airTemp = change.after.child("Temperature").val();
   const waterTemp = change.after.child("WaterTemperature").val();
   const humidity = change.after.child("Humidity").val();
   const tds = change.after.child("TotalDissolvedSolids").val();
   const acidity = change.after.child("pH").val();
-  console.log(airTemp + ' ' + waterTemp + ' ' + FCMTokenSnapshot);
-  
-  //const tokens = ['d2mnsjvQQKuD0td89gfFAD:APA91bE3bm09uzPZgogCSiZpx5HZRPrhwDNuWPDzzgr4JjnuhPBcwCZscBbn7uGCglPwCopmTy2yPlcRcez0K_9qKF5eybkY8NbjiioXda-An4fb6o-Uo4Crwh5EioyuhsvjbLdWFc0k', 'eLLBXN2MTXSzcKTC4jHIxD:APA91bG91zHQZe4ZfwFLBR1bbHg0I34tBerODUZXQLEuxQz6Wm6TlGsHfWJrMxJzG4Cnb0rkZjmOPMWh1ivsITD1bA8Z6gMRPraZYDNOFCbzaBuW1Couj58yWL97n2Eygs4fvJoMAm0j', 'cbq9-IJGRCK9AxYqP1VrJm:APA91bEEhxnbAmg_CWWYiadDAZQzrWkeBSnJSdtDIM4fI2GMqlZN78WGAikBoWathrHF8A16zxOnSSbigifGujcWqrvtWdRERD9oeLRapa9bx1KVMNMu38Or_XPMjcM3SKRPa4UGRkUW'];
+  console.log(airTemp + ' ' + waterTemp + ' ' + FCMToken);
 
   const airTempCheck = airTemp >= 35 || airTemp < 18;
   const humidCheck = humidity >= 85 || humidity < 50;
@@ -27,30 +27,26 @@ exports.sendNotif = refDev.onWrite(async (change, context) => {
   const acidCheck = acidity >= 6.5 || acidity < 5.0;
 
   if (airTempCheck || humidCheck || watTempCheck || tdsCheck || acidCheck) {
-    const payload = {
+    const message = {
       token: FCMToken,
       notification: {
         title: 'Your plants are in danger!',
         body: 'Check on your reservoir and follow the suggestion to save them!',
-        sound: "beep",
-        channel_id: "DigiHydro",
-        //token: "eLLBXN2MTXSzcKTC4jHIxD:APA91bG91zHQZe4ZfwFLBR1bbHg0I34tBerODUZXQLEuxQz6Wm6TlGsHfWJrMxJzG4Cnb0rkZjmOPMWh1ivsITD1bA8Z6gMRPraZYDNOFCbzaBuW1Couj58yWL97n2Eygs4fvJoMAm0j",
-        priority: "high",
+        //sound: "beep",
+        //channel_id: "DigiHydro",
+        //priority: "high",
       },
-      //topic: "notifications",
-      //  Replace with the topic you want to send the notification to
       data: {
         //  Optional data payload to send additional info to the Flutter app
         click_action: 'FLUTTER_NOTIFICATION_CLICK',
       },
     };
-    //console.log(FCMToken);
 
-    admin.messaging().send(payload).then(async (response) => {
-      console.log('Notification sent successfully', response);
-      return null;
-    }).catch((error) => {
-      return {error: error.code};
-    });
+    try {
+      await admin.messaging().send(message);
+      console.log('Notification sent successfully');
+    } catch (error) {
+      console.error('Error sending FCM notification:', error);
+    }
   };
 });
